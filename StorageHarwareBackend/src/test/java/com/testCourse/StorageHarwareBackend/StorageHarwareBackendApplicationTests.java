26b34e6ee98b758b78474be9f73afabbbecc01ae
yeraytorralba <yertormorc@gmail.com>
2018-05-07 11:40:41 +0100

    Add files via upload
    back working , hello world already did

---
+++ StorageHarwareBackend/src/test/java/com/testCourse/StorageHarwareBackend/StorageHarwareBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testCourse.StorageHarwareBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StorageHarwareBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
